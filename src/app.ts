8d3a6741b7e94092c10262e402261f533d321e72
MD Sadid <sfhsadid@gmail.com>
2024-11-30 08:25:09 +0300

    My first comment

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+import { UserRoutes } from './app/modules/user/user.route';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api/users', UserRoutes);
+
+const test = (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+};
+
+app.get('/', test);
+
+app.use(globalErrorHandler);
+
+//Not Found
+app.use(notFound);
+
+export default app;
