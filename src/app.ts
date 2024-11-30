fe995629a52c0de2eb13c5e87b044adc91eafa0b
MD Sadid <sfhsadid@gmail.com>
2024-11-30 09:14:31 +0300

    app router update done

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,6 @@ import express, { Application, Request, Response } from 'express';
 import globalErrorHandler from './app/middlewares/globalErrorhandler';
 import notFound from './app/middlewares/notFound';
 import router from './app/routes';
-import { UserRoutes } from './app/modules/user/user.route';
 
 const app: Application = express();
 
@@ -16,14 +15,14 @@ app.use(express.json());
 app.use(cors());
 
 // application routes
-app.use('/api/users', UserRoutes);
+app.use('/api/v1', router);
 
 const test = (req: Request, res: Response) => {
   const a = 10;
   res.send(a);
 };
 
-app.get('/', test);
+app.get('/test', test);
 
 app.use(globalErrorHandler);
 
