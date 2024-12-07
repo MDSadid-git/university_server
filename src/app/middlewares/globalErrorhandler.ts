9e8870b784353a818e1f4b320fa37f4e983b45b3
MD Sadid <sfhsadid@gmail.com>
2024-12-07 10:37:50 +0300

    handle Cast error done

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -6,6 +6,7 @@ import { ZodError } from 'zod';
 import handleZodError from '../errors/handleZodError';
 import config from '../config';
 import handleValidationError from '../errors/handleValidationError';
+import handleCastError from '../errors/handleCastError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res) => {
   let statusCode = err.statusCode || 500;
@@ -23,6 +24,16 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res) => {
     errorSources = simplifiedError?.errorSources;
   } else if (err?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
